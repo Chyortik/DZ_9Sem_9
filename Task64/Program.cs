6aa24b2332b7b2967e7b3879b3cb4b3cef64f09c
Dmitryi <dimon_smirnov81@mail.ru>
2022-07-24 12:56:55 +0300

    Complete Homework for Seminar 9

---
+++ Task64/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Выполнить задачи обязательно с помощью рекурсии!
+Задача 64: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+Console.Clear();
+Console.Write("Введите m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine()!);
+int temp = m;
+
+if (m > n)
+{
+    m = n;
+    n = temp;
+}
+
+PrintSum(m, n, temp = 0);
+
+void PrintSum(int m, int n, int sum)
+{
+    sum = sum + n;
+    if (n <= m)
+    {
+        Console.Write($"Сумма натуральных элементов = {sum} ");
+        return;
+    }
+    PrintSum(m, n - 1, sum);
+}
+
---
+++ Task66/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Выполнить задачи обязательно с помощью рекурсии!
+Задача 66: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+*/
+Console.Clear();
+Console.Write("Введите m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int functionAkkerman = Ack(m, n);
+
+Console.Write($"Функция Аккермана = {functionAkkerman} ");
+
+int Ack(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
+}
